c7b02b59aa9abade9b9e195684d0c97ae73b56e3
marabend <143610328+marabend@users.noreply.github.com>
2024-10-24 09:27:12 +0200

    Create Skeleton.java

---
+++ 04_Console_Commands/Skeleton.java
@@ -0,0 +1,33 @@
+package org.setup.minecraft;
+
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skeleton extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        getCommand("test").setExecutor(new TestCommand());
+
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        e.setCancelled(true);
+        e.getPlayer().sendMessage(ChatColor.RED + "Stop moving! You are frozen.");
+    }
+
+    @EventHandler
+    public void onPlayerEggThrow(PlayerEggThrowEvent e) {
+        e.getPlayer().sendMessage("You just threw an egg");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
