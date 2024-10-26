1304fc4a2945ed01fc65ecc8afa07917c9b87dbb
marabend <143610328+marabend@users.noreply.github.com>
2024-10-26 03:26:07 +0200

    Create Main.java

---
+++ 06_Permissions/Main.java
@@ -0,0 +1,12 @@
+package org.setup.minecraft;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("test").setExecutor(new TestCommand());
+
+    }
+}
