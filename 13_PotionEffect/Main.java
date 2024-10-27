b3c13f23ded3252d401fbc51557da38513dc8cab
marabend <143610328+marabend@users.noreply.github.com>
2024-10-27 10:39:00 +0100

    Create Main.java

---
+++ 13_PotionEffect/Main.java
@@ -0,0 +1,23 @@
+package org.setup.minecraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public final class Main extends JavaPlugin implements Listener {
+
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent e) {
+        e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 200, 3));
+    }
+}
