e18083c1304332775909668b3ff07da0efa68ce9
marabend <143610328+marabend@users.noreply.github.com>
2025-06-27 11:16:21 +0200

    Update Main.java

--- 23_Banners/Main.java
+++ 23_Banners/Main.java
@@ -4,7 +4,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public final class Main extends JavaPlugin implements Listener {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
