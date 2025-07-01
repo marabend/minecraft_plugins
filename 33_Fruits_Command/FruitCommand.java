03b05522587da4cff9c2ec10315167b59d5767f1
marabend <143610328+marabend@users.noreply.github.com>
2025-07-01 16:01:42 +0200

    Create FruitCommand.java

---
+++ 33_Fruits_Command/FruitCommand.java
@@ -0,0 +1,13 @@
+package org.setup.minecraft;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class FruitCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        return false;
+    }
+}
