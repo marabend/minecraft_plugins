cd0076423245eda818e3ee5c3ae5e6ff96ca3d13
marabend <143610328+marabend@users.noreply.github.com>
2025-07-03 10:04:35 +0200

    Update Main.java

--- 37_Custom_Skulls/Main.java
+++ 37_Custom_Skulls/Main.java
@@ -1,67 +1,14 @@
 package org.setup.minecraft;
 
-import org.bukkit.Material;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.lang.reflect.Field;
-import java.util.UUID;
+public final class Main extends JavaPlugin implements Listener {
 
-public class SkullCommand implements CommandExecutor {
-
-    // TODO: For this check out freshcoal.com
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-
-
-        if(commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-
-            ItemStack is = new ItemStack(Material.PLAYER_HEAD);
-            SkullMeta meta = (SkullMeta) is.getItemMeta();
-            // Might only work if you use dependency spigot instead of spigot-api
-            GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-            profile.getProperties().put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZDQzZDRiN2FjMjRhMWQ2NTBkZGY3M2JkMTQwZjQ5ZmMxMmQyNzM2ZmMxNGE4ZGMyNWMwZjNmMjlkODVmOGYifX19"));
-            Field field;
-
-            try {
-                field = meta.getClass().getDeclaredField("profile");
-                field.setAccessible(true);
-                field.set(meta, profile);
-            } catch(NoSuchFieldException | IllegalAccessException e) {
-                e.printStackTrace();
-                return false;
-            }
-
-            is.setItemMeta(meta);
-
-            player.getInventory().addItem(is);
-        }
-        return false;
+    public void onEnable() {
+        getCommand("skull").setExecutor(new SkullCommand());
     }
 
-    // Doesn't seem to work
-    /*
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-
-
-        if(commandSender instanceof Player) {
-            Player player = (Player) commandSender;
-
-            ItemStack is = new ItemStack(Material.PLAYER_HEAD);
-            SkullMeta meta = (SkullMeta) is.getItemMeta();
-            meta.setOwningPlayer(player);
-            is.setItemMeta(meta);
-
-            player.getInventory().addItem(is);
-        }
-        return false;
-    }
-     */
 }
