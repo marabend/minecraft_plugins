31fd2fa104bddc74940e73bb2341dce37ece4eac
marabend <143610328+marabend@users.noreply.github.com>
2025-07-05 08:16:09 +0200

    Create Main.java

---
+++ 42_persistent_data_containers/Main.java
@@ -0,0 +1,40 @@
+package org.setup.minecraft;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.block.data.type.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        /*
+        NamespacedKey key = new NamespacedKey(this, "surname");
+        Player player = Bukkit.getPlayer("playerName");
+        PersistentDataContainer pdc = player.getPersistentDataContainer();
+        pdc.set(key, PersistentDataType.STRING, "king");
+
+        if(player.getPersistentDataContainer().has(key, PersistentDataType.STRING)) {
+            String surname = player.getPersistentDataContainer().get(key, PersistentDataType.STRING);
+        }
+         
+
+      Sign sign = (Sign)Bukkit.getWorld("world").getBlockAt(1,1,1).getState();
+        sign.getPersistentDataContainer();
+        sign.update();
+        
+         */
+        
+    }
+
+
+}
