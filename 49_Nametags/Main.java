f47a04854f2be28b3c014babe565a92e922aadbb
marabend <143610328+marabend@users.noreply.github.com>
2025-07-07 08:10:46 +0200

    Create Main.java

---
+++ 49_Nametags/Main.java
@@ -0,0 +1,15 @@
+package org.setup.minecraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new NameTagListener(), this);
+    }
+
+
+}
