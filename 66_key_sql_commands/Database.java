ec2b11f655d0e28edea11112453a2e607e5cb143
marabend <143610328+marabend@users.noreply.github.com>
2025-07-09 13:44:56 +0200

    Create Database.java

---
+++ 66_key_sql_commands/Database.java
@@ -0,0 +1,34 @@
+package org.setup.minecraft;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+
+
+    private Connection connection;
+
+    public void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_plugin?useSSL=false", "user", "password");
+    }
+
+    public boolean isConnected() {
+        return connection != null;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void disconnect() {
+        if(isConnected()) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+}
