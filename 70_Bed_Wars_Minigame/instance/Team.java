a729b178dc7970fb074a87efe48cec123beafc5d
marabend <143610328+marabend@users.noreply.github.com>
2025-07-11 10:27:12 +0200

    Create Team.java

---
+++ 70_Bed_Wars_Minigame/instance/Team.java
@@ -0,0 +1,20 @@
+package org.setup.minecraft.instance;
+
+public enum Team {
+
+    RED("Red"),
+    BLUE("Blue"),
+    GREEN("Green"),
+    YELLOW("Yellow");
+
+    private String name;
+
+    Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
