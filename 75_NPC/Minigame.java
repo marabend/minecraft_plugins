c2d164d0ec4cec10b104b011e692b323c53f761b
marabend <143610328+marabend@users.noreply.github.com>
2025-07-12 22:06:31 +0200

    Create Minigame.java

---
+++ 75_NPC/Minigame.java
@@ -0,0 +1,41 @@
+package org.setup.minecraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.setup.minecraft.command.ArenaCommand;
+import org.setup.minecraft.instance.Arena;
+import org.setup.minecraft.listener.ConnectListener;
+import org.setup.minecraft.listener.GameListener;
+import org.setup.minecraft.manager.ArenaManager;
+import org.setup.minecraft.manager.ConfigManager;
+
+
+public class Minigame extends JavaPlugin  {
+
+    private ArenaManager arenaManager;
+
+    @Override
+    public void onEnable() {
+        ConfigManager.setupConfig(this);
+        arenaManager = new ArenaManager(this);
+
+        Bukkit.getPluginManager().registerEvents(new ConnectListener(this), this);
+        Bukkit.getPluginManager().registerEvents(new GameListener(this), this);
+
+        getCommand("arena").setExecutor(new ArenaCommand(this));
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        for(Arena arena : arenaManager.getArenas()) {
+            arena.getNPC().remove();
+        }
+    }
+
+    public ArenaManager getArenaManager() {
+        return arenaManager;
+    }
+
+}
