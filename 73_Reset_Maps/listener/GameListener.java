0faceae0549d57759e4de62dbd30527245c5aa0d
marabend <143610328+marabend@users.noreply.github.com>
2025-07-12 21:02:34 +0200

    Create GameListener.java

---
+++ 73_Reset_Maps/listener/GameListener.java
@@ -0,0 +1,38 @@
+package org.setup.minecraft.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.world.WorldLoadEvent;
+import org.setup.minecraft.GameState;
+import org.setup.minecraft.Minigame;
+import org.setup.minecraft.instance.Arena;
+
+public class GameListener implements Listener {
+
+    private Minigame minigame;
+
+    public GameListener(Minigame minigame) {
+        this.minigame = minigame;
+    }
+
+
+    @EventHandler
+    public void onWorldLoad(WorldLoadEvent e) {
+
+        Arena arena = minigame.getArenaManager().getArena(e.getWorld());
+        if(arena != null) {
+            arena.toggleCanJoin();
+        }
+
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        Arena arena = minigame.getArenaManager().getArena(e.getPlayer());
+
+        if(arena != null && arena.getState().equals(GameState.LIVE)) {
+            arena.getGame().addPoint(e.getPlayer());
+        }
+    }
+}
