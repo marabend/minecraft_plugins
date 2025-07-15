ecab0d0676c60ce21d88fc246d96386d229551d6
marabend <143610328+marabend@users.noreply.github.com>
2025-07-15 10:13:59 +0200

    Create CosmeticsUI.java

---
+++ 83_cosmetics_data_save/navigation/CosmeticsUI.java
@@ -0,0 +1,36 @@
+package org.setup.minecraft.navigation;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class CosmeticsUI {
+
+    public CosmeticsUI(Player player) {
+        Inventory inv = Bukkit.createInventory(null, 27, ChatColor.GOLD.toString() + ChatColor.BOLD + "Cosmetics Menu");
+
+        ItemStack hats = new ItemStack(Material.DIAMOND_HELMET);
+        ItemMeta hatsMeta = hats.getItemMeta();
+        hatsMeta.setDisplayName(ChatColor.AQUA.toString() + ChatColor.BOLD + "Hats");
+        hatsMeta.setLore(Arrays.asList(ChatColor.GRAY + "Puts custom skull on your head!"));
+        hats.setItemMeta(hatsMeta);
+
+        inv.addItem(hats);
+
+        ItemStack trails = new ItemStack(Material.DIAMOND_HELMET);
+        ItemMeta trailsMeta = trails.getItemMeta();
+        trailsMeta.setDisplayName(ChatColor.RED.toString() + ChatColor.BOLD + "Trails");
+        trailsMeta.setLore(Arrays.asList(ChatColor.GRAY + "Spawn cool effects as you walk!"));
+        trails.setItemMeta(trailsMeta);
+
+        inv.addItem(trails);
+
+        player.openInventory(inv);
+    }
+}
