5087128a5296a153cc8e25ba81ccbd45eeb763a1
marabend <143610328+marabend@users.noreply.github.com>
2025-07-15 07:45:29 +0200

    Create Cosmetics.java

---
+++ 80_Cosmetics/Cosmetics.java
@@ -0,0 +1,32 @@
+package org.setup.minecraft;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.setup.minecraft.instance.Cosmetic;
+import org.setup.minecraft.navigation.CosmeticListener;
+import org.setup.minecraft.navigation.CosmeticsCommand;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+
+public class Cosmetics extends JavaPlugin  {
+
+    private HashMap<UUID, List<Cosmetic>> activeCosmetics = new HashMap<>();
+
+    @Override
+    public void onEnable() {
+        getCommand("cosmetics").setExecutor(new CosmeticsCommand());
+        Bukkit.getPluginManager().registerEvents(new CosmeticListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    public HashMap<UUID, List<Cosmetic>> getActiveCosmetics() {
+        return activeCosmetics;
+    }
+}
